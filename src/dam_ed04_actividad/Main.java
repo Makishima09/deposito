f67cc412cbb72e6dbaac31b511022a20e630d2c0
Miguel <Miguel@DESKTOP-DC1R1PA>
2020-02-04 18:20:53 +0000

    cuarto commit

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -11,22 +11,21 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
-        float cantidad;
-        
+                
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
         
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(2300);
 
         
     }
     
-    public static void operativa_cuenta(CCuenta cuenta1) {
+    public static void operativa_cuenta(float cantidad) {       
+        CCuenta cuenta1 = null;
         
-
         try {
-            cuenta1.retirar(2300);
+            cuenta1.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
